e373c406af9acfa159956d3c3eab712b213f478a
Hugo Torquato <hugortorquato@gmail.com>
2024-10-14 14:11:52 -0300

    How to define a install path to put the executable

---
+++ Cmake/Trim/main.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+int main(int argc, char** argv){
+    int count = 1;
+    std::string name;
+
+    while (count < argc)
+    {
+        name += argv[count++];
+    }
+
+    // erase spaces, new lines and things like that
+    name.erase(0,name.find_first_not_of(" \t\n\r\f\v"));
+    name.erase(name.find_last_not_of(" \t\n\r\f\v") + 1);
+
+    std::cout << name << std::endl;
+    return 0;
+    
+}
