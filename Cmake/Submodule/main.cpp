8e4c6d98032baaf0af9f1eb1cb14df62bc172814
Hugo Torquato <hugortorquato@gmail.com>
2024-10-15 10:14:29 -0300

    Init Submodules example, solution using a local lib reference

---
+++ Cmake/Submodule/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Adder/adder.h"
+
+int main() {
+    std::cout << "Hey Budy " << std::endl;
+    std::cout << "2 + 3 = " << mearlymath::add(2, 3) << std::endl;
+    return 0;
+}
