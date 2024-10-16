f124818b403a2fa390177b54087b335f2deecf3d
Hugo Torquato <hugortorquato@gmail.com>
2024-10-16 10:58:48 -0300

    Initial project for testing

---
+++ Cmake/Ctests/Ctests/Ctests.cpp
@@ -0,0 +1,13 @@
+﻿// Ctests.cpp : Defines the entry point for the application.
+//
+
+#include "Ctests.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
+ng
