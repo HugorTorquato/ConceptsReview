9dc382f3495a5ed00609f8cecefb67c3df675f06
Hugo Torquato <hugortorquato@gmail.com>
2024-10-16 08:45:28 -0300

    Initial VS project example

---
+++ Cmake/DemoBase/DemoBase.cpp
@@ -0,0 +1,12 @@
+﻿// DemoBase.cpp : Defines the entry point for the application.
+//
+
+#include "DemoBase.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
