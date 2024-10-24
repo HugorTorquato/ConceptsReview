951ec3dcb28c865b8d628004a45bee83eba36506
Hugo Torquato <hugortorquato@gmail.com>
2024-10-23 21:11:36 -0300

    Refectored to reduce the methods length

--- Algorithms/algorithm-design/DataStructure/src/Trees/src/HelperMethods.cpp
+++ Algorithms/algorithm-design/DataStructure/src/Trees/src/HelperMethods.cpp
@@ -1,4 +1,4 @@
-
+#include <queue>
 
 void includeVertice(
     const int &value, 
@@ -25,4 +25,75 @@ void includeVertice(
         //Include the new vertice in the map
         BTree[key] = vertice;   
 
+}
+
+bool includeElementInTheRootPositionOfTheTreeIfEmpty(
+    const std::shared_ptr<BTreeVertice> BTV,
+    std::shared_ptr<BTreeVertice> &rootVertice){
+
+    if(rootVertice == nullptr){
+        rootVertice = BTV;
+        return true;
+    }
+
+    return false;
+}
+
+void includeElementInTheTreeLeftToRight(
+    const std::shared_ptr<BTreeVertice> BTV,
+    std::shared_ptr<BTreeVertice> &rootVertice    
+){
+    std::queue<std::shared_ptr<BTreeVertice>> treeQueue;
+    treeQueue.push(rootVertice);
+
+    while(!treeQueue.empty()){
+        std::shared_ptr<BTreeVertice> verticeToEvaluate = treeQueue.front();
+        treeQueue.pop();
+
+        std::shared_ptr<BTreeVertice>leftChild = verticeToEvaluate->getLeftChild();
+        std::shared_ptr<BTreeVertice>rightChild = verticeToEvaluate->getRightChild();
+
+        //If the dequeued node’s left child is NULL:
+        if(leftChild == nullptr){            
+            //    Insert the new node as the left child of the dequeued node.
+            //    Break the loop.
+            verticeToEvaluate->setLeftChild(BTV);
+            break;
+        }
+
+        //If the dequeued node’s right child is NULL:
+        if(rightChild == nullptr){            
+            //    Insert the new node as the left child of the dequeued node.
+            //    Break the loop.
+            verticeToEvaluate->setRightChild(BTV);
+            break;
+        }
+
+        treeQueue.push(leftChild);
+        treeQueue.push(rightChild);   
+    }
+}
+
+int retrieveNumberOfNodesInTree(
+    std::shared_ptr<BTreeVertice> &rootVertice
+){
+    int count = 0;
+
+    std::queue<std::shared_ptr<BTreeVertice>> treeQueue;
+    treeQueue.push(rootVertice);
+
+    while(!treeQueue.empty()){
+        std::shared_ptr<BTreeVertice> verticeToEvaluate = treeQueue.front();
+        treeQueue.pop();
+
+        std::shared_ptr<BTreeVertice>leftChild = verticeToEvaluate->getLeftChild();
+        std::shared_ptr<BTreeVertice>rightChild = verticeToEvaluate->getRightChild();
+
+        if(leftChild != nullptr) {treeQueue.push(leftChild);};
+        if(rightChild != nullptr) {treeQueue.push(rightChild);};
+        
+        count++;
+    }
+
+    return count;
 }
--- Algorithms/algorithm-design/DataStructure/src/Trees/src/Trees.cpp
+++ Algorithms/algorithm-design/DataStructure/src/Trees/src/Trees.cpp
@@ -85,68 +85,15 @@ std::shared_ptr<BTreeVertice> BTreesClass::getRootVertice(){
 }
 
 int BTreesClass::getNumberOfNodes(){
-    int count = 0;
-
-    std::queue<std::shared_ptr<BTreeVertice>> treeQueue;
-    treeQueue.push(rootVertice);
-
-    while(!treeQueue.empty()){
-        std::shared_ptr<BTreeVertice> verticeToEvaluate = treeQueue.front();
-        treeQueue.pop();
-
-        std::shared_ptr<BTreeVertice>leftChild = verticeToEvaluate->getLeftChild();
-        std::shared_ptr<BTreeVertice>rightChild = verticeToEvaluate->getRightChild();
-
-        if(leftChild != nullptr) {treeQueue.push(leftChild);};
-        if(rightChild != nullptr) {treeQueue.push(rightChild);};
-        
-        count++;
-    }
-
-    return count;
+    return retrieveNumberOfNodesInTree(rootVertice);
 }
 
 void BTreesClass::includeVerticeAtTree(const std::shared_ptr<BTreeVertice> BTV){
     // At this moment, the Vertice is already initialized and connected to the respected child
     
-    // Verify if the tree is empty
-    if(rootVertice == nullptr){
-        rootVertice = BTV;
+    if(includeElementInTheRootPositionOfTheTreeIfEmpty(BTV, rootVertice)){
         return;
     }
 
-    std::queue<std::shared_ptr<BTreeVertice>> treeQueue;
-    treeQueue.push(rootVertice);
-
-    while(!treeQueue.empty()){
-        std::shared_ptr<BTreeVertice> verticeToEvaluate = treeQueue.front();
-        treeQueue.pop();
-
-        std::shared_ptr<BTreeVertice>leftChild = verticeToEvaluate->getLeftChild();
-        std::shared_ptr<BTreeVertice>rightChild = verticeToEvaluate->getRightChild();
-
-        //If the dequeued node’s left child is NULL:
-        if(leftChild == nullptr){            
-            //    Insert the new node as the left child of the dequeued node.
-            //    Break the loop.
-            verticeToEvaluate->setLeftChild(BTV);
-            break;
-        }
-
-        //If the dequeued node’s right child is NULL:
-        if(rightChild == nullptr){            
-            //    Insert the new node as the left child of the dequeued node.
-            //    Break the loop.
-            verticeToEvaluate->setRightChild(BTV);
-            break;
-        }
-
-        treeQueue.push(leftChild);
-        treeQueue.push(rightChild);
-
-        
-    }
-
-
-
+    includeElementInTheTreeLeftToRight(BTV, rootVertice);
 }
