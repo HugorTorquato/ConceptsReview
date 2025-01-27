4187e68cd44fbe2eba5cd05b66ad4d58ec2f1fef
Hugo Torquato <hugortorquato@gmail.com>
2025-01-27 19:28:17 -0300

    7 - How the C++ Linker Works

--- C++/src/Dev/HelloWorld/src/Log.cpp
+++ C++/src/Dev/HelloWorld/src/Log.cpp
@@ -4,3 +4,8 @@
 void Logr(const char* message) {
     std::cout << message << std::endl;
 }
+
+// This code is node valid
+// void Logr(const char* message) {
+//     std::cout << message << std::endl;
+// }
--- C++/src/Dev/HelloWorld/src/main.cpp
+++ C++/src/Dev/HelloWorld/src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "Log.h"
+#include "Math.cpp"
 
 // Just the declaration.... Need to add to the linker
-// void Log(const char* message);
+void Log(const char* message);
 
 int main()
 {
     Logr("Hello World!");
+    
+
+    std::cout << Multply( 5, 8) << std::endl ;
     std::cin.get();
 }
