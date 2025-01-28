61f35c0099c261f41316eed7dca6100dcd1cf977
Hugo Torquato <hugortorquato@gmail.com>
2025-01-28 07:33:12 -0300

    8 - Variables in C++

--- C++/src/Dev/HelloWorld/src/main.cpp
+++ C++/src/Dev/HelloWorld/src/main.cpp
@@ -3,13 +3,33 @@
 #include "Math.cpp"
 
 // Just the declaration.... Need to add to the linker
-void Log(const char* message);
+// void Log(const char* message);
 
 int main()
 {
     Logr("Hello World!");
     
+    int varable = 8;
+    std::cout << varable << std::endl;
 
-    std::cout << Multply( 5, 8) << std::endl ;
+    char a = 'A';
+    char b = 65;
+    char c = 65;
+    float d = 5.5f; // 4 bytes
+    float e = 5.5; // 8 bytes
+    bool f = false; // 1 bytes
+    std::cout << a << std::endl; // A
+    std::cout << b << std::endl; // A
+    std::cout << c << std::endl; // 65
+    std::cout << d << std::endl; // 5.5
+    std::cout << e << std::endl; // 5.5
+    std::cout << f << std::endl; // 0
+    std::cout << sizeof(bool) << std::endl; // 1
+
+    // unsigner, short, int , long, long long
+
+    // Logr((int)varable);
+
+    std::cout << Multplly( 5, 8) << std::endl ;
     std::cin.get();
 }
